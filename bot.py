86c137215d52859ebfa023e54d9d89d2fc283bc6
MightySpaceman <83145315+MightySpaceman@users.noreply.github.com>
2022-10-30 20:13:23 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import datetime
 import time
 import datetime
 from datetime import datetime
@@ -6,28 +5,33 @@ from dateutil import relativedelta
 from datetime import date
 from mastodon import Mastodon
 
-# get two dates
-d1 = date.today().strftime('%d/%m/%Y')
-d2 = '25/2/2023'
+def post():
+    d1 = date.today().strftime('%d/%m/%Y')
+    d2 = '25/2/2023'
 
-# convert string to date object
-start_date = datetime.strptime(d1, "%d/%m/%Y")
-end_date = datetime.strptime(d2, "%d/%m/%Y")
+    # convert string to date object
+    start_date = datetime.strptime(d1, "%d/%m/%Y")
+    end_date = datetime.strptime(d2, "%d/%m/%Y")
 
-# Get the relativedelta between two dates
-delta = relativedelta.relativedelta(end_date, start_date)
+     # Get the relativedelta between two dates
+    delta = relativedelta.relativedelta(end_date, start_date)
 
-delta2 = start_date - end_date
+    delta2 = start_date - end_date
 
-content = 'Time until KSP 2 release:\n'
-content += f'{delta.months} months, {delta.days} days'
+    content = 'TESTING AUTO-RUN CAPABILITY\n\n'
+    content += 'Time until KSP 2 release:\n'
+    content += f'{delta.months} months, {delta.days} days'
+    content += f'\n\nT{delta2.days} DAYS'
 
-content += f'\n\nT{delta2.days} DAYS'
+    #   Set up Mastodon
+    mastodon = Mastodon(
+        access_token = '[Bot Access Key]', # Your Mastodon bot access key
+        api_base_url = 'https://mstdn.social/' # Base URL of Mastodon instance of choice
+    )
 
-#   Set up Mastodon
-mastodon = Mastodon(
-    access_token = 'token',
-    api_base_url = 'https://mstdn.social/'
-)
+    mastodon.status_post(content) 
 
-mastodon.status_post(content) 
+while True:
+    if (datetime.now().hour == 7) and (datetime.now().minute == 10):
+        post()
+        time.sleep(1000)
