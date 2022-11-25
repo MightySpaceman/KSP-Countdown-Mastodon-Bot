4042769ad6fcb8268f769a2226ce6dee4da8eea0
MightySpaceman <83145315+MightySpaceman@users.noreply.github.com>
2022-11-25 19:45:14 +1100

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,12 +5,6 @@ from dateutil import relativedelta
 from datetime import date
 from mastodon import Mastodon
 
-    #   Set up Mastodon
- mastodon = Mastodon(
-    access_token = 'ng1xe9-rf25zYLyqg2XcoSeWZx7uuAjpfn9g-gI9Pf4', # Your Mastodon bot access key
-    api_base_url = 'https://mstdn.social/' # Base URL of Mastodon instance of choice
-)
-
 def post():
     d1 = date.today().strftime('%d/%m/%Y')
     d2 = '25/2/2023'
@@ -29,9 +23,13 @@ def post():
     content += f'{delta.months} months, {delta.days} days'
     content += f'\n\nT{delta2.days} DAYS'
 
+    #   Set up Mastodon
+    mastodon = Mastodon(
+        access_token = 'ng1xe9-rf25zYLyqg2XcoSeWZx7uuAjpfn9g-gI9Pf4', # Your Mastodon bot access key
+        api_base_url = 'https://mstdn.social/' # Base URL of Mastodon instance of choice
+    )
+
     mastodon.status_post(content) 
-    
-mastodon.status_post(content) 
 
 while True:
     if (datetime.now().hour == 7) and (datetime.now().minute == 10) and (datetime.now().second == 10):
