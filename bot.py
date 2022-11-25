46d7ba1ad9530f0a97268e949607d86f8c868118
MightySpaceman <83145315+MightySpaceman@users.noreply.github.com>
2022-11-25 19:41:13 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,12 @@ from dateutil import relativedelta
 from datetime import date
 from mastodon import Mastodon
 
+    #   Set up Mastodon
+ mastodon = Mastodon(
+    access_token = 'ng1xe9-rf25zYLyqg2XcoSeWZx7uuAjpfn9g-gI9Pf4', # Your Mastodon bot access key
+    api_base_url = 'https://mstdn.social/' # Base URL of Mastodon instance of choice
+)
+
 def post():
     d1 = date.today().strftime('%d/%m/%Y')
     d2 = '25/2/2023'
@@ -22,12 +28,6 @@ def post():
     content += 'Time until KSP 2 release:\n'
     content += f'{delta.months} months, {delta.days} days'
     content += f'\n\nT{delta2.days} DAYS'
-
-    #   Set up Mastodon
-    mastodon = Mastodon(
-        access_token = 'ng1xe9-rf25zYLyqg2XcoSeWZx7uuAjpfn9g-gI9Pf4', # Your Mastodon bot access key
-        api_base_url = 'https://mstdn.social/' # Base URL of Mastodon instance of choice
-    )
 
     mastodon.status_post(content) 
     
